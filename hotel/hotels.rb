d25663175dc5aac464a607d1b5e0b6c4972caf7e
Elizabeth Collins <escollins27@gmail.com>
2014-10-29 14:07:57 -0400

    Read in data and print out hotels

--- hotel/hotels.rb
+++ hotel/hotels.rb
@@ -2,13 +2,17 @@ require "csv"
 
 class Hotel
 
-  def initialize(file name)
-  @file_name = file name
+  def initialize(file_name)
+    @file_name = file_name
   end
 
   def run
     CSV.foreach(@file_name, {headers: true}) do |row|
 
+      puts row["Hotel"]
     end
   end
 end
+
+hotel = Hotel.new("hotels.csv")
+hotel.run
